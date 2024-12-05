1fccb5f034632ec7eb596badedcd455c5bdbcd3e
michaelwang18 <145706779+michaelwang18@users.noreply.github.com>
2024-12-05 06:14:23 -0500

    Check For Question game working

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,6 +33,16 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
+    sourceSets {
+        getByName("main") {
+            assets {
+                srcDirs("src\\main\\assets", "src\\main\\assets",
+                    "src\\main\\assets",
+                    "src\\main\\assets\\standardTrig"
+                )
+            }
+        }
+    }
 }
 
 dependencies {
--- app/src/main/java/com/example/myapplication/SecondFragment.kt
+++ app/src/main/java/com/example/myapplication/SecondFragment.kt
@@ -1,19 +1,22 @@
 package com.example.myapplication
 
 import android.os.Bundle
+import android.provider.ContactsContract.CommonDataKinds.Im
 import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.NavController
 import androidx.navigation.NavDestination
 import androidx.navigation.findNavController
+import org.w3c.dom.Text
 
-class SecondFragment : Fragment(), NavController.OnDestinationChangedListener  {
+class SecondFragment : Fragment() {
 
     private lateinit var textView: TextView
     private lateinit var question: TextView
@@ -22,8 +25,17 @@ class SecondFragment : Fragment(), NavController.OnDestinationChangedListener  {
     private lateinit var answer1: TextView
     private lateinit var answer2: TextView
     private lateinit var answer3: TextView
+    private lateinit var AD1: ImageView
+    private lateinit var AD2: ImageView
+    private lateinit var AD3: ImageView
+    private lateinit var questionImage: ImageView
+
     private var allQuestions: Array<String> = arrayOf("1","2","3","4","5","6")
+
+    private var allQImage: Array<String> = arrayOf("1","2","3","4","5","6")
     private var allAnswers: Array<String> = arrayOf("1","2","3","4","5","6")
+    private var allAImage: Array<String> = arrayOf("1","2","3","4","5","6")
+    private var answers: Array<Int> = arrayOf(1,2,3)
     private var currentScore = 0
     private var highScore = 0
     private var questionID = -1
@@ -37,11 +49,13 @@ class SecondFragment : Fragment(), NavController.OnDestinationChangedListener  {
         val view: View = inflater.inflate(R.layout.fragment_second, container, false)
         textView = view.findViewById(R.id.textview_fragment2)
         question = view.findViewById(R.id.question)
-        answer1 = view.findViewById(R.id.answer1)
-        answer2 = view.findViewById(R.id.answer2)
-        answer3 = view.findViewById(R.id.answer3)
+
+        answer1 = view.findViewById(R.id.answer1);  answer2 = view.findViewById(R.id.answer2);  answer3 = view.findViewById(R.id.answer3)
+        AD1 = view.findViewById(R.id.ADis1);        AD2 = view.findViewById(R.id.ADis2);        AD3 = view.findViewById(R.id.ADis3)
+        AD1.scaleType = ImageView.ScaleType.CENTER; AD2.scaleType = ImageView.ScaleType.CENTER; AD3.scaleType = ImageView.ScaleType.CENTER
         score = view.findViewById(R.id.score)
-        maxScore = view.findViewById(R.id.maxScore)
+
+        maxScore = view.findViewById(R.id.maxScore) //Takes from main activity, Single Source of Truth Thingy
         val activity: MainActivity = context as MainActivity
         highScore = activity.getHighScore2()
         return view
@@ -74,15 +88,16 @@ class SecondFragment : Fragment(), NavController.OnDestinationChangedListener  {
 
         question.setOnClickListener{
             changeQuestion()
-            chooseAnswer(questionID)
+
+           // chooseAnswer(questionID) //Temp to Test
         }
 
-
-
-
-
-
-
+        AD1.setOnClickListener{ chooseAnswer(1) }
+        AD2.setOnClickListener{ chooseAnswer(2) }
+        AD3.setOnClickListener{ chooseAnswer(3) }
+       // answer1.setOnClickListener{ chooseAnswer(1) }
+        //answer2.setOnClickListener{ chooseAnswer(2) }
+        //answer3.setOnClickListener{ chooseAnswer(3) }
 
 
 
@@ -99,13 +114,76 @@ class SecondFragment : Fragment(), NavController.OnDestinationChangedListener  {
 
     }
 
+
+
+
     fun changeQuestion(){
+        var tempList = allQuestions
         var currentQuestion = (Math.random() * 6).toInt()
         questionID = currentQuestion
         Log.i("SecondFragment", currentQuestion.toString())
-
         question.text = allQuestions[currentQuestion]
+        //Change the answer:
+
+
+        answers[0] = questionID.toInt()
+        answers[1] = (Math.random() * 6).toInt()
+        answers[2] = (Math.random() * 6).toInt()
+        while (answers[0] == answers[1] || answers[0] == answers[2] || answers[1] == answers[2]){
+            answers[1] = (Math.random() * 6).toInt()
+            answers[2] = (Math.random() * 6).toInt()
+        }
+        answers.shuffle()
+        setImages()
+        var displays: Array<TextView> = arrayOf(answer1,answer2,answer3)
+        for (nums in 0..2){
+            displays[nums].text = answers[nums].toString()
+            //make Actual images
+
+        }
+
+
     }
+
+
+    fun setImages(){
+        //Need to make question Image
+        var answerImages: Array<ImageView> = arrayOf(AD1,AD2,AD3)
+        questionImage.setImageResource(R.drawable.lego_computer)//Test
+
+        if (questionID == 1){ questionImage.setImageResource(R.drawable.q1) }
+        if (questionID == 2){ questionImage.setImageResource(R.drawable.q2) }
+        if (questionID == 3){ questionImage.setImageResource(R.drawable.q3) }
+        if (questionID == 4){ questionImage.setImageResource(R.drawable.q4) }
+        if (questionID == 5){ questionImage.setImageResource(R.drawable.q5) }
+        if (questionID == 6){ questionImage.setImageResource(R.drawable.q6) }
+        
+
+        for(nums in 0..2){
+            answerImages[nums].setImageResource(R.drawable.lego_computer)//Test
+
+            if (answers[nums] == 1){ answerImages[nums].setImageResource(R.drawable.a1) }
+            if (answers[nums] == 2){ answerImages[nums].setImageResource(R.drawable.a2) }
+            if (answers[nums] == 3){ answerImages[nums].setImageResource(R.drawable.a3) }
+            if (answers[nums] == 4){ answerImages[nums].setImageResource(R.drawable.a4) }
+            if (answers[nums] == 5){ answerImages[nums].setImageResource(R.drawable.a5) }
+            if (answers[nums] == 6){ answerImages[nums].setImageResource(R.drawable.a6) }
+        }
+
+
+
+
+
+
+        // TODO:  run through the answer list and set the corresponding imageViews to the right picture
+        //SRC is a string which uhh yea you can cook
+
+
+
+    }
+
+
+
 
     fun chooseAnswer(answerIndex: Int){
         if (answerIndex == questionID){
@@ -114,7 +192,7 @@ class SecondFragment : Fragment(), NavController.OnDestinationChangedListener  {
         {
             currentScore = 0;
         }
-
+        changeQuestion()
 
     }
 
@@ -130,21 +208,6 @@ class SecondFragment : Fragment(), NavController.OnDestinationChangedListener  {
     }
 
 
-
-
-
-
-
-
-
-
-    override fun onDestinationChanged(   //dont think it works
-        controller: NavController,
-        destination: NavDestination,
-        arguments: Bundle?
-    ) {
-        Log.i("Second Frag","Hi")
-    }
 
     override fun onResume() {
         super.onResume()
