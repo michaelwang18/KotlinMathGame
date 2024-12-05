678db5117cac13049f4591863454673796697ad2
michaelwang18 <145706779+michaelwang18@users.noreply.github.com>
2024-12-05 06:14:23 -0500

    Finish up Demo Question

--- app/src/main/java/com/example/myapplication/SecondFragment.kt
+++ app/src/main/java/com/example/myapplication/SecondFragment.kt
@@ -22,6 +22,8 @@ class SecondFragment : Fragment(), NavController.OnDestinationChangedListener  {
     private lateinit var answer1: TextView
     private lateinit var answer2: TextView
     private lateinit var answer3: TextView
+    private var allQuestions: Array<String> = arrayOf("1","2","3","4","5","6")
+    private var allAnswers: Array<String> = arrayOf("1","2","3","4","5","6")
 
 
     override fun onCreateView(
@@ -31,20 +33,50 @@ class SecondFragment : Fragment(), NavController.OnDestinationChangedListener  {
         // Inflate the layout for this fragment
         val view: View = inflater.inflate(R.layout.fragment_second, container, false)
         textView = view.findViewById(R.id.textview_fragment2)
-        return view
-    }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
         question = view.findViewById(R.id.question)
         answer1 = view.findViewById(R.id.answer1)
         answer2 = view.findViewById(R.id.answer2)
         answer3 = view.findViewById(R.id.answer3)
         score = view.findViewById(R.id.score)
         maxScore = view.findViewById(R.id.maxScore)
+        return view
+
+
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val args: Bundle? = arguments // synthetic property from getArguments/setArguments
+        if (args != null) {
+            val strArg: String? = args.getString("name")
+            val intArg: Int = args.getInt("faveNumber") // returns 0 rather than null
+            val textView: TextView = view.findViewById(R.id.textview_fragment2) // textView2 is ID for the textView in the third fragment (yikes forgot to change this ID)
+            textView.text = "$strArg $intArg"
+        }
+
+
+
+
 
         val secondToThirdButton: Button = view.findViewById(R.id.button_two_to_three)
         val navController: NavController = view.findNavController()
+
+
+        changeQuestion()
+
+
+        question.setOnClickListener{
+            changeQuestion()
+        }
+
+
+
+
+
+
+
 
 
 
@@ -54,6 +86,18 @@ class SecondFragment : Fragment(), NavController.OnDestinationChangedListener  {
             data.putInt("faveNumber", 3)
             navController.navigate(R.id.navigate_second_to_third, data)
         }
+
+
+
+    }
+
+    fun changeQuestion(){
+        var currentQuestion = (Math.random() * 6).toInt()
+        Log.i("SecondFragment", currentQuestion.toString())
+
+        question.text = allQuestions[currentQuestion]
+
+
 
 
 
